5b43c146e59f74a2f50e5b629c4e51bb8b6a5c5b
sudais73 <sudaisman193@gmail.com>
2025-08-08 20:12:24 +0300

    update frontend url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend//
 
 const placeOrder = async(req,res)=>{
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://full-stack-food-delivery-web.vercel.app"
 try {
     const newOrder = new orderModel({
         userId:req.user.userId,
@@ -122,6 +122,7 @@ const updateStatus = async(req,res)=>{
   }
 
 }
+
 
 
 export  {placeOrder,verifyOrder,userOrders,listOrders,updateStatus}
